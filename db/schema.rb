5b6c0ae03576edb4405ff64f434b9c90a5cbc1b6
yunari26 <uawasiran3@gmail.com>
2021-11-21 12:10:10 +0000

    station01

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_115107) do
+ActiveRecord::Schema.define(version: 2021_11_21_120612) do
 
   create_table "movies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "image_url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "year"
+    t.string "description"
+    t.boolean "is_showing"
   end
 
 end
